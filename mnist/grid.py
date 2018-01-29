0913fc8639df24fd6768ccd1aaa65a299e416780
takanokage <danut.petre@gmail.com>
2018-01-29 13:26:33 -0800

    Update copyright.
    The initial formulation belongs to the TensorFlow team. However this is new work.

--- mnist/grid.py
+++ mnist/grid.py
@@ -1,16 +1,7 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Well, this is based on the original work by the TensorFlow team:
+# tensorflow/examples/tutorials/mnist/mnist_deep.py
+# I believe they should get their due credit for the that work.
+# This is new work.
 # ==============================================================================
 
 """A deep MNIST classifier using convolutional layers.
